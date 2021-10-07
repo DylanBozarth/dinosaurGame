4bd8426b379005c3be731ea94668503e2b346fff
dylan <dylanabozarth@gmail.com>
2021-10-07 16:44:19 -0700

    endgame screen 1

---
+++ src/gameover.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+export const GameOver = () => {
+    return (
+        <div>a</div>
+    )
+}
--- src/jumpgame.js
+++ src/jumpgame.js
@@ -1,47 +1,48 @@
 import React, { useState, useEffect } from "react";
 import "./games.css";
+
+import useInterval from "react-useinterval";
 export const JumpGame = () => {
   const [score, setScore] = useState(0);
   const [enemy, setEnemy] = useState({});
-  const [playerClass, setPlayerClass] = useState(" ");
-
+  const [playerClass, setPlayerClass] = useState("");
+  useInterval(check4Death, 3000);
+  function check4Death() {
+    if (playerClass !== "") {
+      setScore(score + 1);
+    } else {
+      GameOver();
+    }
+  }
   document.addEventListener("keyup", (event) => {
     if (event.code === "Space") {
       Jump();
     }
-// fires around 100 times a second
-    function check4Death() {
-      if (playerClass !== "") {
-        console.log("safe");
-      } else {
-        console.log("nope");
-        clearInterval();
-      }
-    }
-    setInterval(check4Death, 3000);
-
-    // the score counter is just a timer with the same delay as the enemy animation
-    const ScoreCount = () => {
-      setTimeout(() => {
-        setScore(score + 1);
-      }, 3000);
-    };
-    ScoreCount();
   });
 
   function Jump() {
     setPlayerClass("jump");
     setTimeout(() => {
       setPlayerClass("");
-    }, 500);
+    }, 900);
   }
+  const GameOver = () => {
+    alert(`You went extinct! Your score was ${score}`);
+    clearInterval(check4Death);
+    setScore(0);
+  };
   return (
     <div className="masterDiv">
-      <div className="jumpgameBox">
-        Try and survive lol
-        <div className={`player ${playerClass}`} id="player"></div>
-        <div style={enemy} className="enemy" id="enemy"></div>
+      {" "}
+      
+     
+      <div className="jumpgameBox"><div className="titlebox">
+        <h3 className="score">Don't go extinct!</h3>
+        <br />
         <p className="score">Score: {score}</p>
+      </div>
+        <div className={`player ${playerClass}`} id="player"></div>
+        <div style={enemy} className="enemy" id="enemy"></div><div className="fire"></div>
       </div>
     </div>
   );
