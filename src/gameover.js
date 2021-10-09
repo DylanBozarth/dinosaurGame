1e4b9b12d8520fb4fa2b3afb44bc243e20a7610b
dylan <dylanabozarth@gmail.com>
2021-10-09 09:18:18 -0700

    interaction

--- src/gameover.js
+++ src/gameover.js
@@ -6,11 +6,22 @@ export const GameOver = (props) => {
     
     useEffect(() => {
         props.sadMusic.play()
+        const playPromise = props.sadMusic.play();
+        if (playPromise !== undefined) {
+          playPromise
+            .then(() => {
+               console.log("audio played auto");
+            })
+            .catch(() => {
+               console.log("playback prevented");
+            });
+        }
+       
     })
     return (
         
         <div className="gameOverScreen">
-
+<button onClick={() => props.sadMusic.play()}>aaa</button>
 <h2 className="deadText">YOU DIED <br /> YOUR DEEDS SHALL BE REMEMBERED <br />
 UNTIL YOU REFRESH THE PAGE</h2>
 <Link to="/dino" className="start">Try again?</Link>
